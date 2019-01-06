87f2b008d564c4f8615c312686b6f0add395201a
Serdar SANRI <ssanri@aol.com>
2019-01-05 23:07:29 -0500

    EditProduct.js
    Remember that form has to be it's own container and stateless.

--- server/client/src/components/products/Editproduct.js
+++ server/client/src/components/products/Editproduct.js
@@ -1,86 +1,55 @@
 import React,{Component} from 'react'
 import {reduxForm,Field} from "redux-form"
-import * as actions from "../../actions"
-import {compose} from "redux"
-import {connect} from "react-redux"
-import  requireAuth from "../requireAuth"
 
-class Editproduct extends Component{
-   
-    onSubmit=(formProps)=>{
-        this.props.editProduct(formProps,()=>{
-            this.props.history.push("/home")
-        })
-    }
-    
-    render(){
-        const editPro = this.props.location.state
-        console.log(editPro)
+const Editproduct (props) => {
+   const { onSubmit, handleSubmit, pristine, submitting, invalid } = props;
+   return(
+      <form onSubmit={handleSubmit(onSubmit)}>
+           <div className="form-group">
+           <label><b>Product Name</b></label>
+            <Field
+              name="name"
+              type="text"
+              component="input"
+              autoComplete="none"
+              className = "form-control"
+            />
+           </div>
+           <div className="form-group">
+           <label><b>Category</b></label>
+            <Field
+              name="category"
+              type="text"
+              component="input"
+              autoComplete="none"
+              className = "form-control"
+            />
+           </div>
+           <div className="form-group">
+           <label><b>Quantity</b></label>
+            <Field
+              name="quantity"
+              type="number"
+              component="input"
+              autoComplete="none"
+              className = "form-control"
+            />
+           </div>
+           <div className="form-group">
+           <label><b>Price</b></label>
+            <Field
+              name="price"
+              type="number"
+              component="input"
+              autoComplete="none"
+              className="form-control"
+            />
+           </div>
+           <button className="btn btn-primary" disabled={pristine|invalid|submitting} >Add Product</button>
 
-         const data={
-            id:editPro.id,
-            name:editPro.name,
-            category:editPro.category,
-            price:editPro.price,
-            quantity:editPro.quantity
-         }
-         console.log(data)
-        const {handleSubmit}=this.props
-        return(
-            <form onSubmit={handleSubmit(this.onSubmit)}>
-                 <div className="form-group">
-                 <label><b>Product Name</b></label>
-                  <Field
-                    name="name"
-                    type="text"
-                    component="input"
-                    autoComplete="none"
-                    className = "form-control"
-                  />
-                 </div>
-                 <div className="form-group">
-                 <label><b>Category</b></label>
-                  <Field
-                    name="category"
-                    type="text"
-                    component="input"
-                    autoComplete="none"
-                    className = "form-control"
-                  />
-                 </div>
-                 <div className="form-group">
-                 <label><b>Quantity</b></label>
-                  <Field
-                    name="quantity"
-                    type="number"
-                    component="input"
-                    autoComplete="none"
-                    className = "form-control"
-                  />
-                 </div>
-                 <div className="form-group">
-                 <label><b>Price</b></label>
-                  <Field
-                    name="price"
-                    type="number"
-                    component="input"
-                    autoComplete="none"
-                    className="form-control"
-                  />
-                 </div>
-                 <button className="btn btn-primary" >Add Product</button>
-                 
-            </form>
-        )
-    }
-
+      </form>
+  );
 }
-function mapStateToProps(){
-
-}
-
-
-export default compose(
-    connect(null, actions),
-     reduxForm({form:"editProduct"})
-    ,requireAuth)(Editproduct)
+export default reduxForm({
+   form: 'editProduct'
+})
