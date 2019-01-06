807e19e9e2e8bdcbde9f43b60de1b940c220d73d
Serdar SANRI <ssanri@aol.com>
2019-01-05 23:14:28 -0500

    Update EditProductContainer.js

--- server/client/src/components/products/EditProductContainer.js
+++ server/client/src/components/products/EditProductContainer.js
@@ -1,41 +1,30 @@
 import React,{Component} from 'react'
-import {reduxForm,Field} from "redux-form"
 import * as actions from "../../actions"
 import {compose} from "redux"
 import {connect} from "react-redux"
 import  requireAuth from "../requireAuth"
 
-class Editproduct extends Component{
+class EditProductContainer extends Component{
    
-    onSubmit=(formProps)=>{
-        this.props.editProduct(formProps,()=>{
-            this.props.history.push("/home")
+    onSubmit=(values)=>{
+        this.props.editProduct(values, () => {
+            this.props.history.push("/home");
         })
     }
     
     render(){
-        const editPro = this.props.location.state
-        console.log(editPro)
-
-         const data={
-            id:editPro.id,
-            name:editPro.name,
-            category:editPro.category,
-            price:editPro.price,
-            quantity:editPro.quantity
-         }
-         console.log(data)
-        const {handleSubmit}=this.props
-        
+         return (
+            <EditProductForm onSubmit={this.onSubmit} initialData={this.props.initialData}/>
+         );        
     }
-
 }
-function mapStateToProps(){
-  
+const mapStateToProps = (state, ownProps) => {   
+   return {
+      initialData: {...ownProps.editPro}
+   }
 }
 
 
 export default compose(
-    connect(null, actions),
-     reduxForm({form:"editProduct"})
-    ,requireAuth)(Editproduct)
+    connect(mapStateToProps, actions),
+    ,requireAuth)(EditProductContainer)
